296ed14cd947f219f3b503c0f3d7228b67228c52
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-08 23:15:40 -0700

    Add some seed data for users table and delete schema

---
+++ db/seeds.rb
@@ -0,0 +1,45 @@
+user1 = {
+  first_name: 'Harry',
+  last_name: 'Potter',
+  username: 'hpotter7',
+  email: 'hpotter7@gmail.com',
+  password: 'a'
+}
+
+user2 = {
+	first_name: 'Hermione',
+	last_name: 'Granger',
+	username: 'hgranger7',
+	email: 'hgranger7@gmail.com',
+	password: 'a'
+}
+
+user3 = {
+	first_name: 'Ronald',
+	last_name: 'Weasley',
+	username: 'ron.wealsey7',
+	email: 'ron.wealsey@gmail.com',
+	password: 'a'
+}
+
+wand1 = {
+	name: '',
+	core: '',
+	wood_type: ''
+}
+
+wand2 = {
+	name: '',
+	core: '',
+	wood_type: ''
+}
+
+wand3 = {
+	name: '',
+	core: '',
+	wood_type: ''
+}
+
+User.create(user1)
+User.create(user2)
+User.create(user3)
