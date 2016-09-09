ee1b40ab70c5807dc2f94d478c49fb4d7237eee0
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-08 23:41:07 -0700

    Add seed wand data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,39 +7,67 @@ user1 = {
 }
 
 user2 = {
-	first_name: 'Hermione',
-	last_name: 'Granger',
-	username: 'hgranger7',
-	email: 'hgranger7@gmail.com',
-	password: 'a'
+  first_name: 'Hermione',
+  last_name: 'Granger',
+  username: 'hgranger7',
+  email: 'hgranger7@gmail.com',
+  password: 'a'
 }
 
 user3 = {
-	first_name: 'Ronald',
-	last_name: 'Weasley',
-	username: 'ron.wealsey7',
-	email: 'ron.wealsey@gmail.com',
-	password: 'a'
+  first_name: 'Ronald',
+  last_name: 'Weasley',
+  username: 'ron.wealsey7',
+  email: 'ron.wealsey@gmail.com',
+  password: 'a'
+}
+
+user4 = {
+  first_name: 'Garrick',
+  last_name: 'Ollivander',
+  username: 'gollivander7',
+  email: 'gollivander7@gmail.com',
+  password: 'a'
 }
 
 wand1 = {
-	name: '',
-	core: '',
-	wood_type: ''
+  length: 11,
+  core: 'Phoenix Feather',
+  wood_type: 'Holly'
 }
 
 wand2 = {
-	name: '',
-	core: '',
-	wood_type: ''
+  length:  10.75,
+  core: 'Dragon Heartstring',
+  wood_type: 'Vine'
 }
 
 wand3 = {
-	name: '',
-	core: '',
-	wood_type: ''
+  length: 14,
+  core: 'Unicorn Hair',
+  wood_type: 'Willow'
+}
+
+wand4 = {
+  length: 12.75,
+  core: 'Dragon Heartstring',
+  wood_type: 'Hornbeam'
 }
 
 User.create(user1)
 User.create(user2)
 User.create(user3)
+User.create(user4)
+
+
+Wand.create(wand1)
+wand = Wand.last
+wand.owner_id = 1
+
+Wand.create(wand2)
+wand = Wand.last
+wand.owner_id = 2
+
+Wand.create(wand3)
+wand = Wand.last
+wand.owner_id = 3
