468fba1cfae889fde89d5e25c56950a2739c955e
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-09 00:05:23 -0700

    Add Article seed data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,26 +33,85 @@ user4 = {
 wand1 = {
   length: 11,
   core: 'Phoenix Feather',
-  wood_type: 'Holly'
+  wood_type: 'Holly',
+  owner_id: 1,
+  creator_id: 4
+
 }
 
 wand2 = {
   length:  10.75,
   core: 'Dragon Heartstring',
-  wood_type: 'Vine'
+  wood_type: 'Vine',
+  owner_id: 2,
+  creator_id: 4
 }
 
 wand3 = {
   length: 14,
   core: 'Unicorn Hair',
-  wood_type: 'Willow'
+  wood_type: 'Willow',
+  owner_id: 3,
+  creator_id: 4
 }
 
 wand4 = {
   length: 12.75,
   core: 'Dragon Heartstring',
-  wood_type: 'Hornbeam'
+  wood_type: 'Hornbeam',
+  owner_id: 4,
+  creator_id: 4
 }
+
+account1 = {
+  user_id: 1,
+  balance: 15_000_00,
+  interest_rate: 0.04
+}
+
+account2 = {
+  user_id: 2,
+  balance: 1_000_000,
+  interest_rate: 0.04
+}
+
+account3 = {
+  user_id: 3,
+  balance: 1_000_000,
+  interest_rate: 0.04
+}
+
+account4 = {
+  user_id: 4,
+  balance: 3_000_000,
+  interest_rate: 0.04
+}
+
+article1 = {
+  user_id: 1,
+  article_title: 'Harry Potters Info Page',
+  article_body: "Info about Harry Potter's Stuff"
+}
+
+article2 = {
+  user_id: 2,
+  article_title: 'Hermione Granger Info Page',
+  article_body: "Info about Hermione Granger's Stuff"
+}
+
+article3 = {
+  user_id: 3,
+  article_title: 'Ronald Weasley Info Page',
+  article_body: "Info about Ronald Weasley's Stuff"
+}
+
+article4 = {
+  user_id: 4,
+  article_title: 'Garrick Ollivander Info Page',
+  article_body: "Info about Garrick Ollivander's Stuff"
+}
+
+
 
 User.create(user1)
 User.create(user2)
@@ -61,16 +120,29 @@ User.create(user4)
 
 
 Wand.create(wand1)
-wand = Wand.last
-wand.owner_id = 1
-wand.creator_id = 4
-
 Wand.create(wand2)
-wand = Wand.last
-wand.owner_id = 2
-wand.creator_id = 4
-
 Wand.create(wand3)
-wand = Wand.last
-wand.owner_id = 3
-wand.creator_id = 4
+Wand.create(wand4)
+
+Gringotts_Account.create(account1)
+Gringotts_Account.create(account2)
+Gringotts_Account.create(account3)
+Gringotts_Account.create(account4)
+
+Article.create(article1)
+Article.create(article2)
+Article.create(article3)
+Article.create(article4)
+
+
+
+
+
+
+
+
+
+
+
+
+
