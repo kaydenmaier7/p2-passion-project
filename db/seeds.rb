482dbdfdbfcb53a69ba289f59b83f60f3f0f7b97
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-09 04:30:06 -0700

    Add more user seed data. New sheme_file.xml

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,141 +1,277 @@
-user1 = {
+harry = {
   first_name: 'Harry',
   last_name: 'Potter',
-  username: 'hpotter7',
+  username: 'hpotter1',
   email: 'hpotter7@gmail.com',
-  password: 'a'
+  password: 'a',
+  blood_status: 'Half-blood',
+  species: 'wizard',
+  occupation: 'Head of the Department of Magical Law Enforcement',
+  school_house: 'Gryffindor'
 }
 
-user2 = {
+hermione = {
   first_name: 'Hermione',
   last_name: 'Granger',
-  username: 'hgranger7',
+  username: 'hgranger2',
   email: 'hgranger7@gmail.com',
-  password: 'a'
+  password: 'a',
+  blood_status: 'Muggle-born',
+  species: 'witch',
+  occupation: 'Deputy Head of the Department of Magical Law Enforcement',
+  school_house: 'Gryffindor'
 }
 
-user3 = {
+ron = {
   first_name: 'Ronald',
   last_name: 'Weasley',
-  username: 'ron.wealsey7',
-  email: 'ron.wealsey@gmail.com',
-  password: 'a'
+  username: 'ron.weasley3',
+  email: 'ron.weasley@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: "Co-owner and Co-manager of Weasleys' Wizard Wheezes ",
+  school_house: 'Gryffindor'
 }
 
-user4 = {
+arthur = {
+  first_name: 'Arthur',
+  last_name: 'Weasley',
+  username: 'arthur.weasley4',
+  email: 'arthur.weasley4@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: 'Head of the Office for the Detection and Confiscation of Counterfeit Defensive Spells and Protective Objects',
+  school_house: 'Gryffindor'
+}
+
+molly = {
+  first_name: 'Molly',
+  last_name: 'Weasley',
+  username: 'molly.weasley5',
+  email: 'molly.weasley5@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'witch',
+  occupation: 'Stay At Home Mom',
+  school_house: 'Gryffindor'
+}
+
+bill = {
+  first_name: 'Bill',
+  last_name: 'Weasley',
+  username: 'bill.weasley6',
+  email: 'bill.weasley6@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: 'Curse Breaker for Gringotts Bank',
+  school_house: 'Gryffindor'
+}
+
+charlie = {
+  first_name: 'Charlie',
+  last_name: 'Weasley',
+  username: 'charlie.weasley7',
+  email: 'charlie.weasley7@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: 'Dragonologist
+',
+  school_house: 'Gryffindor'
+}
+
+percy = {
+  first_name: 'Percy',
+  last_name: 'Weasley',
+  username: 'percy.weasley8',
+  email: 'percy.weasley8@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: 'Head of the Department of Magical Transportation',
+  school_house: 'Gryffindor'
+}
+
+fredI = {
+  first_name: 'Fred I',
+  last_name: 'Weasley',
+  username: 'fred.weasley9',
+  email: 'fred.weasley9@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: "Deceased... Former Co-owner and Co-manager of Weasleys' Wizard Wheezes ",
+  school_house: 'Gryffindor'
+}
+
+george = {
+  first_name: 'George',
+  last_name: 'Weasley',
+  username: 'George.weasley10',
+  email: 'George.weasley10@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: "Co-owner and Co-manager of Weasleys' Wizard Wheezes ",
+  school_house: 'Gryffindor'
+}
+
+ginny = {
+  first_name: 'Ronald',
+  last_name: 'Weasley',
+  username: 'Ginny.weasley11',
+  email: 'Ginny.weasley11@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'witch',
+  occupation: 'Sports Editor for the Daily Prophet',
+  school_house: 'Gryffindor'
+}
+
+ollivander = {
   first_name: 'Garrick',
   last_name: 'Ollivander',
-  username: 'gollivander7',
-  email: 'gollivander7@gmail.com',
-  password: 'a'
+  username: 'gollivander12',
+  email: 'gollivander12@gmail.com',
+  password: 'a',
+  blood_status: 'Half-blood',
+  species: 'wizard',
+  occupation: 'Wandmaker',
+  school_house: 'Ravenclaw'
 }
 
-wand1 = {
+sirius = {
+  first_name: 'Sirius',
+  last_name: 'Black',
+  username: 'sirblack13',
+  email: 'sirblack13@gmail.com',
+  password: 'a',
+  blood_status: 'Pure-blood',
+  species: 'wizard',
+  occupation: '',
+  school_house: 'Gryffindor'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+harry_wand = {
   length: 11,
   core: 'Phoenix Feather',
   wood_type: 'Holly',
   owner_id: 1,
-  creator_id: 4
+  creator_id: 12
 
 }
 
-wand2 = {
+hermione_wand = {
   length:  10.75,
   core: 'Dragon Heartstring',
   wood_type: 'Vine',
   owner_id: 2,
-  creator_id: 4
+  creator_id: 12
 }
 
-wand3 = {
+ron_wand = {
   length: 14,
   core: 'Unicorn Hair',
   wood_type: 'Willow',
   owner_id: 3,
-  creator_id: 4
+  creator_id: 12
 }
 
-wand4 = {
+ollivander_wand = {
   length: 12.75,
   core: 'Dragon Heartstring',
   wood_type: 'Hornbeam',
-  owner_id: 4,
-  creator_id: 4
+  owner_id: 12,
+  creator_id: 12
 }
 
-account1 = {
+harry_account = {
   user_id: 1,
   balance: 15_000_00,
   interest_rate: 0.04
 }
 
-account2 = {
+hermione_account = {
   user_id: 2,
   balance: 1_000_000,
   interest_rate: 0.04
 }
 
-account3 = {
+ron_account = {
   user_id: 3,
   balance: 1_000_000,
   interest_rate: 0.04
 }
 
-account4 = {
+ollivander_account = {
   user_id: 4,
   balance: 3_000_000,
   interest_rate: 0.04
 }
 
-article1 = {
+harry_article = {
   user_id: 1,
   article_title: 'Harry Potters Info Page',
   article_body: "Info about Harry Potter's Stuff"
 }
 
-article2 = {
+hermione_article2 = {
   user_id: 2,
   article_title: 'Hermione Granger Info Page',
   article_body: "Info about Hermione Granger's Stuff"
 }
 
-article3 = {
+ron_article3 = {
   user_id: 3,
   article_title: 'Ronald Weasley Info Page',
   article_body: "Info about Ronald Weasley's Stuff"
 }
 
-article4 = {
+ollivander_article4 = {
   user_id: 4,
   article_title: 'Garrick Ollivander Info Page',
   article_body: "Info about Garrick Ollivander's Stuff"
 }
 
+harry_owl = {
+  owner_id: 1,
+  name: 'Hedwig',
+  species: 'Snowy Owl'
 
-
-User.create(user1)
-User.create(user2)
-User.create(user3)
-User.create(user4)
-
-
-Wand.create(wand1)
-Wand.create(wand2)
-Wand.create(wand3)
-Wand.create(wand4)
-
-Gringotts_Account.create(account1)
-Gringotts_Account.create(account2)
-Gringotts_Account.create(account3)
-Gringotts_Account.create(account4)
-
-Article.create(article1)
-Article.create(article2)
-Article.create(article3)
-Article.create(article4)
+}
 
 
 
+ron_owl1 = {
+  owner_id: 3,
+  name: 'Pigwidgeon',
+  species: 'Little Owl'
+
+}
+
+ron_owl2 = {
+  owner_id: 3,
+  name: 'Errol',
+  species: 'Great Grey'
+
+}
 
 
 
@@ -143,6 +279,45 @@ Article.create(article4)
 
 
 
+
+
+
+
+
+
+User.create(harry)
+User.create(hermione)
+User.create(ron)
+User.create(arthur)
+User.create(molly)
+User.create(bill)
+User.create(charlie)
+User.create(percy)
+User.create(fredI)
+User.create(george)
+User.create(ginny)
+User.create(ollivander)
+User.create(sirius)
+
+
+Wand.create(harry_wand)
+Wand.create(hermione_wand)
+Wand.create(ron_wand)
+Wand.create(ollivander_wand)
+
+GringottsAccount.create(harry_account)
+GringottsAccount.create(hermione_account)
+GringottsAccount.create(ron_account)
+GringottsAccount.create(ollivander_account)
+
+Article.create(harry_article)
+Article.create(hermione_article2)
+Article.create(ron_article3)
+Article.create(ollivander_article4)
+
+Owl.create(harry_owl)
+Owl.create(ron_owl1)
+Owl.create(ron_owl2)
 
 
 
