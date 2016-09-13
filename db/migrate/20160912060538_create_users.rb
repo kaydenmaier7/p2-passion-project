84fb066e50edd56dedbab443c64ff62fced8ae84
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-13 13:52:10 -0700

    Finish migration for Users table

--- db/migrate/20160912060538_create_users.rb
+++ db/migrate/20160912060538_create_users.rb
@@ -1,4 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+  	create_table :users do |t|
+  		t.string :first_name, null: false
+  		t.string :last_name, null: false
+  		t.string :username, null: false
+  		t.string :email, null: false
+  		t.string :password_hash, null: false
+			t.string :blood_status, null: false  		
+			t.string :species, null: false
+			t.string :occupation, null: false
+			t.string :school, null: false
+			t.string :school_house, null: false			
+			t.string :patronus, null: false
+
+			t.timestamps			
+  	end
   end
 end
