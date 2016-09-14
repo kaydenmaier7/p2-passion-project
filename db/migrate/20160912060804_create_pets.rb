4126df2240b27dfd24a98aaaf78549136780288c
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-13 21:35:32 -0700

    Finish migration for pet class

--- db/migrate/20160912060804_create_pets.rb
+++ db/migrate/20160912060804_create_pets.rb
@@ -1,4 +1,11 @@
 class CreatePets < ActiveRecord::Migration
   def change
+  	create_table :pets do |t|
+  		t.string :name, null: false
+  		t.string :species, null: false
+  		t.references :owner
+  		
+  		t.timestamps
+  	end
   end
 end
