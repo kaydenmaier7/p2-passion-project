24797ead824ac526ef5effae9cc9ea846b334580
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-13 21:26:22 -0700

    Finish migrations for accounts and wands

--- db/migrate/20160912060715_create_wands.rb
+++ db/migrate/20160912060715_create_wands.rb
@@ -1,4 +1,13 @@
 class CreateWands < ActiveRecord::Migration
   def change
+  	create_table :wands do |t|
+  		t.float :length
+  		t.string :wood_type
+  		t.string :core
+  		t.references :owner
+  		t.references :creator
+
+  		t.timestamps
+  	end
   end
 end
--- db/migrate/20160912060733_create_gringotts_accounts.rb
+++ db/migrate/20160912060733_create_gringotts_accounts.rb
@@ -1,4 +1,11 @@
 class CreateGringottsAccounts < ActiveRecord::Migration
   def change
+  	create_table :gringotts_accounts do |t|
+  		t.decimal :balance, precision: 17, scale: 5
+  		t.decimal :interest_rate, precision: 8, scale: 4
+  		t.references :users
+
+  		t.timestamps
+  	end
   end
 end
