19bc3909fcca0d019e2b70a10cef77ceed1d6415
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-20 13:06:20 -0700

    Add errors method to show in the post route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,9 +19,11 @@ post '/articles' do
   if current_user
     @article = Article.new(params[:article])
     @article.user_id = @current_user.id
+    
     if @article.save
       redirect "/articles/#{@article.id}"
     else
+      errors(@article)
       erb :'articles/new'
     end
   else
