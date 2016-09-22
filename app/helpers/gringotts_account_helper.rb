b3a32326ab61da8ae61d5bbfa31755154397969b
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-22 00:19:00 -0700

    Finish all routes. Make helper methods to refactor code. Add link to show page to update resource

--- app/helpers/gringotts_account_helper.rb
+++ app/helpers/gringotts_account_helper.rb
@@ -1,7 +1,7 @@
 helpers do
-  
+
   def link_gringotts_account(account)
-    @gringotts_account = GringottsAccount.new(params[:gringotts_account])
+    @gringotts_account = GringottsAccount.new(account)
     @gringotts_account.user_id = current_user.id
 
     if @gringotts_account.save
@@ -13,7 +13,45 @@ helpers do
   end
 
   def set_gringotts_account
-  	@gringotts_account = GringottsAccount.find(params[:id])
+    @gringotts_account = GringottsAccount.find(params[:id])
   end
 
+  def authenticated_gringotts_account_show_form
+    if resource_owner(@gringotts_account)
+      erb :'gringotts_accounts/show'
+
+    elsif session[:id] == nil
+      @errors = [
+        'You must be logged in to view this content.'
+      ]
+      erb :'sessions/new'
+
+    else
+      @errors = [
+        'You are not authorized to view this content!',
+        'For security reasons, you are being logged out.'
+      ]
+      logout
+      erb :'sessions/new'
+    end
+  end
+
+  def authenticated_gringotts_account_edit_form
+    if resource_owner(@gringotts_account)
+      erb :'gringotts_accounts/edit'
+
+    elsif session[:id] == nil
+      @errors = ['You must be logged to view this content.']
+      erb :'sessions/new'
+
+    else
+      @errors = [
+        'You are not authorized to view this content!',
+        'For security reasons, you are being logged out.'
+      ]
+      logout
+      erb :'sessions/new'
+    end
+
+  end
 end
