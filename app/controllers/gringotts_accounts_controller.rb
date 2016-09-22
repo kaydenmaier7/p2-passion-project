d9d3ae6a9580024df2ad7b287e688957d568096a
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-21 20:19:37 -0700

    Add post route and form view for Gringotts_account

--- app/controllers/gringotts_accounts_controller.rb
+++ app/controllers/gringotts_accounts_controller.rb
@@ -3,9 +3,14 @@ get '/gringotts_accounts/new' do
 	erb :'gringotts_accounts/new'
 end
  
+# Gringotts Account Create
+post '/gringotts_accounts' do	
+	@gringotts_account = GringottsAccount.new(params[:gringotts_account])
+end
 
 # Account Show
 get '/gringotts_accounts/:id' do
 	@gringotts_account = current_user.gringotts_account if logged_in?
 	erb :'gringotts_accounts/show'
 end
+
