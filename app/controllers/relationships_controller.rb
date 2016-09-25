eaa8311ee2f0c9ddb7fed7241a1a3b8f2cc8c4e9
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-25 13:35:54 -0700

    Delete relationship new and show

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,17 +4,9 @@ get '/users/:user_id/relationships' do
 	erb :'relationships/index'
 end
 
-# Relationships New Form
-	get '/users/:user_id/relationships/new' do
-		@user = User.find(params[:user_id])
-		erb :'relationships/new'
-	end
-
-# Relationships Create
 # Relationships Edit
-# Relationships Show
+
 # Relationships Update
-# Relationships Destroy
 
 
 
