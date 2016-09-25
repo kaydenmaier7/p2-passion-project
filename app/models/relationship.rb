d49ac9ff6ce796ccdb9ad54ee36953747a1e3e9f
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-24 23:29:02 -0700

    Create #relative_info which returns info about a users relative

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,9 @@ class Relationship < ActiveRecord::Base
 	belongs_to :user, dependent: :destroy
 	belongs_to :relative, class_name: 'User'
 	
+	def relative_info
+		User.find(self.relative_id)
+	end
+
+
 end
