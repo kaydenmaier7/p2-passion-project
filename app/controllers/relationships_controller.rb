727859c7ffafb2c73441a51f0b00e37b7dcdc6cd
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-09-25 03:08:22 -0700

    Add index and form routes

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,21 @@
-# Relationship Index For User
+# Relationships Index For User
 get '/users/:user_id/relationships' do
 	@relationships = User.find(params[:user_id]).relationships
 	erb :'relationships/index'
 end
+
+# Relationships New Form
+	get '/users/:user_id/relationships/new' do
+		@user = User.find(params[:user_id])
+		erb :'relationships/new'
+	end
+
+# Relationships Create
+# Relationships Edit
+# Relationships Show
+# Relationships Update
+# Relationships Destroy
+
+
+
+
